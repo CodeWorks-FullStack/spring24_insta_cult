6b11fca8581ac9b0248e6149e3f7a1253807bbd3
Jeremy Fowler <jembyfowler@gmail.com>
2024-05-20 11:31:59 -0600

    storing cults in appstate

--- client/src/services/CultsService.js
+++ client/src/services/CultsService.js
@@ -1,3 +1,5 @@
+import { AppState } from "../AppState.js"
+import { Cult } from "../models/Cult.js"
 import { logger } from "../utils/Logger.js"
 import { api } from "./AxiosService.js"
 
@@ -5,6 +7,7 @@ class CultsService {
   async getCults() {
     const res = await api.get('api/cults')
     logger.log('GOT CULTS 💀💀💀', res.data)
+    AppState.cults = res.data.map(pojo => new Cult(pojo))
   }
 }
 
