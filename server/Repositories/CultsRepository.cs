724a1896c11a825e17b0c6bcd043234dcb89ad9e
Jeremy Fowler <jembyfowler@gmail.com>
2024-05-20 09:52:50 -0600

    set up files

---
+++ server/Repositories/CultsRepository.cs
@@ -0,0 +1,11 @@
+namespace insta_cult.Repositories;
+
+public class CultsRepository
+{
+  private readonly IDbConnection _db;
+
+  public CultsRepository(IDbConnection db)
+  {
+    _db = db;
+  }
+}
---
+++ server/Services/CultsService.cs
@@ -0,0 +1,11 @@
+namespace insta_cult.Services;
+
+public class CultsService
+{
+  private readonly CultsRepository _repository;
+
+  public CultsService(CultsRepository repository)
+  {
+    _repository = repository;
+  }
+}
